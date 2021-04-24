6e836fd6f96bb12ac861924eea574e9dd76e432e
Darshan <shrisohanpal@gmail.com>
2021-04-24 13:41:10 +0530

    images serving from backend

--- backend/server.js
+++ backend/server.js
@@ -34,12 +34,16 @@ app.use('/api/products', productRoutes)
 app.use('/api/orders', orderRoutes)
 app.use('/api/upload', uploadRoutes)
 
+
 app.get('/api/config/paypal', (req, res) =>
     res.send(process.env.PAYPAL_CLIENT_ID)
 )
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+app.use(express.static('public'));
+app.use('/api/uploads', express.static('uploads'))
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '/frontend/build')))
@@ -48,8 +52,7 @@ if (process.env.NODE_ENV === 'production') {
         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
     )
 } else {
-    app.get('/', (req, res) =>
-    {
+    app.get('/', (req, res) => {
         res.send('API is running....')
     })
 }
--- expoapp/urls.js
+++ expoapp/urls.js
@@ -1,3 +1,4 @@
 const baseUrl = 'https://kalpvriksh.herokuapp.com/'
+//const baseUrl = 'http:localhost:5000/'
 
 export { baseUrl }
