b0ffa5364d4bbcb33a7cd669a614eb2ba36a7473
Darshan <shrisohanpal@gmail.com>
2021-04-25 10:37:02 +0530

    images added

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
+import { Card, Image } from 'react-bootstrap'
 
-const Product = ({ product }) =>
-{
+const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
             <Link to={`/product/${product._id}`}>
-                <Card.Img src={product.images[0]} variant='top' />
-
+                <Image style={{ display: 'block', width: '100%', height: 150 }} src={product.images[0]} variant='top' fluid />
                 <Card.Body>
                     <Card.Title as='div'>
                         <strong>{product.name}</strong>
--- frontend/src/components/Shop.js
+++ frontend/src/components/Shop.js
@@ -1,13 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Card } from 'react-bootstrap'
+import { Card, Image } from 'react-bootstrap'
 
-const Shop = ({ shop }) =>
-{
+const Shop = ({ shop }) => {
     return (
-        <Card className='my-3 p-3 rounded'>
+        <Card className='my-3 p-1 rounded' style={{ display: 'block', width: '100%', height: 250 }}>
             <Link to={`/shopscreen/${shop._id}`}>
-                <Card.Img src={shop.image} variant='top' />
+                <Image style={{ display: 'block', width: '100%', height: 150 }} src={shop.image} variant='top' fluid />
                 <Card.Body>
                     <Card.Title as='div'>
                         <strong>{shop.name}</strong>
